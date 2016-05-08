570735059dda1ad89c4c993ddbf278db3fdcc069
Craig Salzsieder <craig.salzsieder@gmail.com>
2016-05-08 16:00:22 -0600

    add locations post

--- public/src/app.js
+++ public/src/app.js
@@ -5,9 +5,9 @@ export class App {
     config.title = 'Aurelia';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'locations',     name: 'locations',    moduleId: 'locations',    nav: true, title: 'Create Locations' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      //{ route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
+      { route: 'location',     name: 'location',    moduleId: 'location',    nav: true, title: 'Create Locations' },
+      //{ route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
     ]);
 
     this.router = router;
--- public/src/location.js
+++ public/src/location.js
@@ -11,16 +11,40 @@ export class Locations {
   previousLocations = [];
 
   constructor(http) {
-    
+    http.configure(config => {
+      config
+        .useStandardConfiguration()
+        .withBaseUrl('http://localhost:7000/locations/user')
+         .withDefaults({
+            headers: {
+              'content-type': 'application/json',
+            }
+          });
+    });
+
+    this.http = http;
   }
 
   activate() {
   }
 
   submit(){
+
+    var location = {
+      userId: 1,
+      name: this.name,
+      description: this.description,
+      longitude: this.longitude,
+      latitude: this.latitude
+    }
   	//make api call to save form
-    var test = this.name;
-    var test2 = this.description;
+    var fetch = this.http.fetch('', {
+      method: 'post',
+      body: JSON.stringify(location)
+    }).then(response =>{
+      this.previousLocations.push(location);
+      
+    });
 
   	//if success add location data to previous locations
   }
