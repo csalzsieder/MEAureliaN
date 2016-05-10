4b5efdc7cec27532dcbd2ecec29c0619008769c6
Craig Salzsieder <craig.salzsieder@gmail.com>
2016-05-09 22:45:43 -0600

    removed unsued files

--- public/src/app.js
+++ public/src/app.js
@@ -3,11 +3,10 @@
 export class App {
   configureRouter(config, router) {
     config.title = 'Home';
-	//config.addPipelineStep('authorize', AuthStep);
 
     config.map([
       { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Login' },
-      { route: 'location',     name: 'location', moduleId: 'location',nav: true, auth: true, title: 'Create Locations' },
+      { route: 'location',     name: 'location', moduleId: 'location',nav: true, title: 'Create Locations' },
     ]);
 
     this.router = router;
